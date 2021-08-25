df75836332e99fc0c2ef378fa117f678a4de60f4
ryanjoshuachildress <ryanjoshuachildress@gmail.com>
2021-08-25 11:40:21 -0700

    Google Auth Working

--- app/src/main/java/com/ryanjoshuachildress/everyonesprojectmanagement/MainActivity.kt
+++ app/src/main/java/com/ryanjoshuachildress/everyonesprojectmanagement/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.ryanjoshuachildress.everyonesprojectmanagement
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Base64.encodeToString
+import android.util.Log
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvName: TextView
+
+    val auth = FirebaseAuth.getInstance().currentUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvName = findViewById(R.id.tvName)
+        if(auth == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+        }else{
+            tvName.text = auth.displayName
+        }
     }
+
 }
